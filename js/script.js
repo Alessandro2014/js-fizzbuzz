bc0fda6c38c4e469b84281fca895837dc23f44a9
Alessandro2014 <alessandromerenda22@gmail.com>
2021-07-05 17:21:45 +0200

    inserimento parole chiavi

--- js/script.js
+++ js/script.js
@@ -3,12 +3,19 @@ Scrivi un programma che stampi i numeri da 1 a 100,
 ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-1far stampare i numeri da 1 a 100
-2 creare condizioni affinchè vengano modificati i multipli di 3 e 5 in parole chiavi*/
+1 far stampare i numeri da 1 a 100
+2 creare varianti con le parole chiavi
+3 creare condizioni affinchè vengano modificati i multipli di 3 e 5 in parole chiavi
+*/
 
-
-var exsercise = document.getElementById("exsercise");
 
 for (var i = 1; i <= 100; i++) {
     console.log("numero ", i);
+    if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz");
+}   if (i % 3 == 0) {
+    console.log("Fizz");
+}   else if (i % 5 == 0) {
+    console.log("Buzz");
+}    
 }
